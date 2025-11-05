d250835b79825e9bd14a1e7514cc1508cc4cc549
zimolab <zimolab@aliyun.com>
2025-11-05 23:40:39 +0800

    add a utility function

--- amake/utils.py
+++ amake/utils.py
@@ -55,6 +55,31 @@ def move_to_desktop_center(window: Union[tkinter.Tk, tkinter.Toplevel]):
     window.deiconify()
 
 
+def move_to_center_of(
+    window: Union[tkinter.Tk, tkinter.Toplevel],
+    ref_window: Optional[Union[tkinter.Tk, tkinter.Toplevel]] = None,
+):
+    """将窗口移动到另一个窗口的中心位置"""
+    if ref_window is None:
+        move_to_desktop_center(window)
+        return
+    window.withdraw()
+    window.update()
+    ref_window.update()
+    x = (
+        ref_window.winfo_rootx()
+        + ref_window.winfo_width() // 2
+        - window.winfo_width() // 2
+    )
+    y = (
+        ref_window.winfo_rooty()
+        + ref_window.winfo_height() // 2
+        - window.winfo_height() // 2
+    )
+    window.geometry(f"+{int(x)}+{int(y)}")
+    window.deiconify()
+
+
 def open_file_in_editor(file_path: Union[str, Path]):
     """
     使用系统默认文本编辑器打开文件
