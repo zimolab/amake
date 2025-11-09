2800eb90e55388f9ecaa757038162185caca9cb9
zimolab <zimolab@aliyun.com>
2025-11-09 18:37:53 +0800

    use new method to get path in package

--- amake/_assets.py
+++ amake/_assets.py
@@ -1,27 +1,20 @@
 from pathlib import Path
 from typing import Union
 
+from pyguiadapterlite.pathutils import package_path
+
 _PACKAGE_NAME = "amake"
 _ASSETS_DIR_NAME = "_assets"
 _LOCALES_DIR_NAME = "locales"
 _IMAGES_DIR_NAME = "images"
 
 
+def _assets_path(package: str, data_dir: str, *paths) -> Path:
+    return package_path(package, data_dir, *paths)
+
+
 def assets_dir(path: Union[str, Path, None] = None):
-    if isinstance(path, str):
-        path = path.strip()
-    path = path or ""
-    try:
-        from importlib.resources import as_file, files
-
-        with as_file(files(_PACKAGE_NAME).joinpath(_ASSETS_DIR_NAME, path)) as path:
-            return path
-    except ImportError:
-        import pkg_resources
-
-        return Path(
-            pkg_resources.resource_filename(_PACKAGE_NAME, _ASSETS_DIR_NAME)
-        ).joinpath(path)
+    return _assets_path(_PACKAGE_NAME, _ASSETS_DIR_NAME, path)
 
 
 def locales_dir() -> Path:
